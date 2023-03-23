2727dfcc590c669265f0b75db53b5e4283badf0e
yuunbou <112618391+yuunbou@users.noreply.github.co>
2023-03-23 15:14:11 +0000

    [Study]条件分岐の書き方

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-alert('Hello' + 'World');
+let orange = 150;
+let apple = 100;
 
-let str1 = 'Hello';
-let str2 = 'World!!!'
-alert(str1 + str2);
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごの値段が同じ');
+} else{
+  alert('みかんの値段がりんごより高い');
+} 
