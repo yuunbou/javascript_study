81d9053360ec8d6a7349e972bee6ea1094916cb6
yuunbou <112618391+yuunbou@users.noreply.github.co>
2023-04-24 14:54:07 +0000

    [Add]保存された内容で動作を切り替える

--- sample.js
+++ sample.js
@@ -1,5 +1,7 @@
 'use strict';
 
 // 入力に応じて動作を変更する
-let answer = window.prompt('ヘルプを見ますか？');
-console.log(answer);
+const answer = window.prompt('ヘルプを見ますか？');
+if(answer === 'yes') {
+  window.alert('タップでジャンプ、障害物をよけます');
+}
