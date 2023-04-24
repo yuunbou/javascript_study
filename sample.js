48415371b71cfde57062f3a509600b98c2480520
yuunbou <112618391+yuunbou@users.noreply.github.co>
2023-04-24 14:59:46 +0000

    [Study]条件分技

--- sample.js
+++ sample.js
@@ -1,7 +1,11 @@
 'use strict';
 
-// 入力に応じて動作を変更する
-const answer = window.prompt('ヘルプを見ますか？');
-if(answer === 'yes') {
-  window.alert('タップでジャンプ、障害物をよけます');
+// 動作のバリエーションを増やす（条件分岐）
+const anser = window.prompt('ヘルプを見ますか？');
+if(anser === 'yes') {
+  window.alert('タップでジャンプ、障害物をよけます。');
+} else if(anser === 'no') {
+  window.alert('ゲーム起動中...');
+} else {
+  window.alert('yesかnoでお答えください。');
 }
