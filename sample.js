c90912c5f680b3feceaea9c3eef1ebeb35f079bb
yuunbou <112618391+yuunbou@users.noreply.github.co>
2023-04-27 13:32:30 +0000

    [Study]数当てゲーム

--- sample.js
+++ sample.js
@@ -1,11 +1,20 @@
 'use strict';
 
-// 動作のバリエーションを増やす（条件分岐）
-const anser = window.prompt('ヘルプを見ますか？');
-if(anser === 'yes') {
-  window.alert('タップでジャンプ、障害物をよけます。');
-} else if(anser === 'no') {
-  window.alert('ゲーム起動中...');
+// 数当てゲーム
+// ランダムで0~5までの整数を生成し定数numberに代入
+const number = Math.floor(Math.random() * 6);
+// 入力した定数answerに代入している
+// parseIntメソッドを使っていったん整数に変換し、代入
+const answer = parseInt(window.prompt('数当てゲーム。0〜5の数字を入力してね。'));
+
+let message;
+if(answer === number) {
+  message = '当たり!';
+} else if(answer < number) {
+  message = '残念でした！もっと大きいです！';
+} else if(answer > number) {
+  message = '残念でした！もっと小さいです！';
 } else {
-  window.alert('yesかnoでお答えください。');
+  message = '0〜５の数字を入力してね';
 }
+window.alert(message);
