aa48f5644eef8a7d9f8b64ddc8d3915b63796ee0
yuunbou <112618391+yuunbou@users.noreply.github.co>
2023-05-06 14:38:08 +0000

    [Staudy]値を入力してモンスターを倒す

--- sample3.js
+++ sample3.js
@@ -1,16 +1,10 @@
-let attack = prompt('1〜110の数値を入力してください');
 // モンスターの体力を作成
 let monsterLife = 100;
 
-// TODO: if文とモンスターの体力と入力された数値の引き算を入れ結果がアラートとして出るように
-monsterLife -= attack; 
-
-// alert(`モンスターに攻撃！モンスターに${attack}のダメージ！モンスターの残り体力は${monsterLife}です`)
-
-if(monsterLife <= 90) {
-    alert('まだまだです！');
-} else if(monsterLife <= 40) {
-    alert('大ダメージ！');
-} else if(monsterLife === 0) {
-    alert('モンスターを倒した！');
+while(monsterLife > 0) {
+    let attack = prompt('1〜110の数値を入力してください');
+    monsterLife -= attack
+    alert(`モンスターに攻撃！モンスターに${attack}のダメージ！モンスターの残り体力は${monsterLife}です`);
 }
+
+alert('モンスターを倒した！')
