c2e831ad47f00dbd5527ff6901eb7851e6158748
yuunbou <112618391+yuunbou@users.noreply.github.co>
2023-05-11 14:33:18 +0000

    [Study] FizzBuzz作成

--- sample3.js
+++ sample3.js
@@ -2,9 +2,9 @@
 let monsterLife = 100;
 
 while(monsterLife > 0) {
-    let attack = prompt('1〜110の数値を入力してください');
+    let attack = prompt('1〜100の数値を入力してください');
     monsterLife -= attack
     alert(`モンスターに攻撃！モンスターに${attack}のダメージ！モンスターの残り体力は${monsterLife}です`);
 }
 
-alert('モンスターを倒した！')
+alert('モンスターを倒した！');
