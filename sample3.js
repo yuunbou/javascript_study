c7951fc31b855205e5ecde5f9f90053eb1a87e42
yuunbou <112618391+yuunbou@users.noreply.github.co>
2023-05-13 14:43:09 +0000

    [Study]todo配列をHTMLに表示

--- sample3.js
+++ sample3.js
@@ -1,9 +1,11 @@
 'use strict';
 
 // 配列を作成
-let todo = ['デザインカンプ作成', 'データ整理', '勉強会申込み', '牛乳を買う']
-
+let todo = ['デザインカンプ作成', 'データ整理', '勉強会申込み', '牛乳を買う'];
+// 配列を追加
+todo.push('歯医者に行く');
 //  配列の各項目全て読み取る
 for (let item of todo) {
-    console.log(item);
+    const li = `<li>${item}</li>`;
+    document.getElementById('list').insertAdjacentHTML('beforeend', li);
 }
