51abd01f24cf171133dfb011f3e3aeb7a3381b2a
yuunbou <112618391+yuunbou@users.noreply.github.co>
2023-05-21 15:10:15 +0000

    [Study] HTMLに出力

--- sample3.js
+++ sample3.js
@@ -2,8 +2,7 @@
 
 // アイテムの価格と在庫を表示する
 let jsbook = {title: 'JavaScript入門', price: 2500, stock: 3};
-console.log(jsbook);
-console.log(jsbook.title);
-console.log(jsbook['price']);
-jsbook.stock = 10;
-console.log(jsbook.stock);
+
+document.getElementById('title').textContent = jsbook.title;
+document.getElementById('price').textContent = jsbook.price + '円';
+document.getElementById('stock').textContent = jsbook.stock;
