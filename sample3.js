d0df04b16a2193bdf95a266ea299b468159631ca
yuunbou <112618391+yuunbou@users.noreply.github.co>
2023-05-29 13:11:05 +0000

    [Study]フォームの入力内容を取得する

--- sample3.js
+++ sample3.js
@@ -1,8 +1,9 @@
 'use strict';
 
-// アイテムの価格と在庫を表示する
-let jsbook = {title: 'JavaScript入門', price: 2500, stock: 3};
-
-document.getElementById('title').textContent = jsbook.title;
-document.getElementById('price').textContent = jsbook.price + '円';
-document.getElementById('stock').textContent = jsbook.stock;
+// フォーム入力の内容を取得する
+// イベントプログラム
+document.getElementById('form').onsubmit = function(event) {
+    event.preventDefault();
+    const search = document.getElementById('form').word.value;
+    document.getElementById('output').textContent = `「${search}」の検索中...`;
+};
