a4a005162eaf078204e4ef5cd06691ec174cc24e
yuunbou <112618391+yuunbou@users.noreply.github.co>
2023-06-08 14:29:01 +0000

    [Study]Mathオブジェクト２

--- sample4.js
+++ sample4.js
@@ -5,4 +5,11 @@
 document.getElementById('pi').textContent = Math.PI;
 document.getElementById('floor').textContent = Math.floor(Math.PI);
 
+function point(num, digit) {
+  const mover = 10 ** digit;
+  return Math.floor(num * mover) / mover;
+}
+
+document.getElementById('output').textContent = point(Math.PI, 2);
+
 
