dede887c424622d23b8501c456fc78199477e00d
yuunbou <112618391+yuunbou@users.noreply.github.co>
2023-06-08 14:08:18 +0000

    [Study]日時表示

--- sample3.js
+++ sample3.js
@@ -7,7 +7,14 @@ const month = now.getMonth();
 const date = now.getDate();
 const hour = now.getHours();
 const min = now.getMinutes();
+let ampm = '';
+if(hour < 12) {
+    ampm = 'a.m';
+} else {
+    ampm = 'p.m';
+}
 
-const output = `${year}/${month + 1}/${date} ${hour}:${min}`;
+
+const output = `${year}/${month + 1}/${date} ${hour % 12}:${min}${ampm}`;
 document.getElementById('time').textContent = output;
 
