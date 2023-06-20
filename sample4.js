7e9e2d8889bdf840f712d846433e80ef1c7448e5
yuunbou <112618391+yuunbou@users.noreply.github.com>
2023-06-20 14:07:56 +0000

    [Study]カウントダウンタイマ-3

--- sample4.js
+++ sample4.js
@@ -14,16 +14,19 @@ function countdown(due) {
   return count;
 }
 
-let goal = new Date();
-goal.setHours(23);
-goal.setMinutes(59);
-goal.setSeconds(59);
+const goal = new Date(2025, 4, 3);
+// goal.setHours(23);
+// goal.setMinutes(59);
+// goal.setSeconds(59);
 
 function recalc() {
-  console.log(countdown(goal));
   const counter = countdown(goal);
-  const time = `${counter[1]}時間${counter[2]}分${counter[3]}秒`;
-  document.getElementById('timer').textContent = time;
+  // const time = `${counter[1]}時間${counter[2]}分${counter[3]}秒`;
+  // document.getElementById('timer').textContent = time;
+  document.getElementById('day').textContent = counter[0];
+  document.getElementById('hour').textContent = counter[1];
+  document.getElementById('min').textContent = String(counter[2]).padStart(2, '0');
+  document.getElementById('sec').textContent = String(counter[3]).padStart(2, '0');
   refresh();
 }
 
